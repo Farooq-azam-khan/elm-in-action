a24bb770a1f225c068d3f71ce98f22c14e474c14
Farooq <farooq1.khan@ryerson.ca>
2022-06-17 22:32:58 -0400

    fixing failing tests

--- tests/PhotoGrooveTests.elm
+++ tests/PhotoGrooveTests.elm
@@ -2,7 +2,20 @@ module PhotoGrooveTests exposing (..)
 
 import Expect exposing (Expectation)
 import Fuzz exposing (Fuzzer, int, list, string)
+import Json.Decode as JD exposing (decodeString)
+import PhotoGroove
 import Test exposing (..)
+
+
+decoderTest : Test
+decoderTest =
+    test "title defaults to (untitle)"
+        (\_ ->
+            """{"url":"fruits.com","size":5}"""
+                |> decodeString PhotoGroove.photoDecoder
+                |> Expect.equal
+                    (Ok { url = "fruits.com", size = 5, title = "(untitle)" })
+        )
 
 
 
@@ -12,8 +25,8 @@ import Test exposing (..)
        -- a -> a -> Expectation
        Expect.equal (1 + 1) 2
 -}
-
-
-suite : Test
-suite =
-    test "one plus one equals two" (\_ -> Expect.equal 2 (1 + 1))
+{-
+   suite : Test
+   suite =
+       test "one plus one equals two" (\_ -> Expect.equal 2 (1 + 1))
+-}
