91d53c484b136f90be1508975982b3ff6255cf9d
Farooq <farooq1.khan@ryerson.ca>
2022-06-17 22:33:25 -0400

    simplification

--- tests/PhotoGrooveTests.elm
+++ tests/PhotoGrooveTests.elm
@@ -9,24 +9,9 @@ import Test exposing (..)
 
 decoderTest : Test
 decoderTest =
-    test "title defaults to (untitle)"
-        (\_ ->
+    test "title defaults to (untitle)" <|
+        \_ ->
             """{"url":"fruits.com","size":5}"""
                 |> decodeString PhotoGroove.photoDecoder
                 |> Expect.equal
                     (Ok { url = "fruits.com", size = 5, title = "(untitle)" })
-        )
-
-
-
-{-
-   expectation : Expectation
-   expectation =
-       -- a -> a -> Expectation
-       Expect.equal (1 + 1) 2
--}
-{-
-   suite : Test
-   suite =
-       test "one plus one equals two" (\_ -> Expect.equal 2 (1 + 1))
--}
