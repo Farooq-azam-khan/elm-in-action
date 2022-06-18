193794e33ec34c9add5ddbcbf7f9077840250f14
Farooq <farooq1.khan@ryerson.ca>
2022-06-17 22:44:12 -0400

    removed (\photo -> photo.title) with .title function

--- tests/PhotoGrooveTests.elm
+++ tests/PhotoGrooveTests.elm
@@ -13,5 +13,5 @@ decoderTest =
         \_ ->
             """{"url":"fruits.com","size":5}"""
                 |> decodeString PhotoGroove.photoDecoder
-                |> Result.map (\photo -> photo.title)
+                |> Result.map .title
                 |> Expect.equal (Ok "(untitle)")
