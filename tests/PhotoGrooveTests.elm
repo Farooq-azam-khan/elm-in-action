fbf582923277bdab2ff07ac98f65747a9e85a94e
Farooq <farooq1.khan@ryerson.ca>
2022-06-17 22:54:42 -0400

    replaced decodeString with decodevalue

--- tests/PhotoGrooveTests.elm
+++ tests/PhotoGrooveTests.elm
@@ -2,7 +2,8 @@ module PhotoGrooveTests exposing (..)
 
 import Expect exposing (Expectation)
 import Fuzz exposing (Fuzzer, int, list, string)
-import Json.Decode as JD exposing (decodeString)
+import Json.Decode as Decode exposing (decodeValue)
+import Json.Encode as Encode
 import PhotoGroove
 import Test exposing (..)
 
@@ -11,7 +12,8 @@ decoderTest : Test
 decoderTest =
     test "title defaults to (untitle)" <|
         \_ ->
-            """{"url":"fruits.com","size":5}"""
-                |> decodeString PhotoGroove.photoDecoder
+            [ ( "url", Encode.string "fruits.com" ), ( "size", Encode.int 5 ) ]
+                |> Encode.object
+                |> decodeValue PhotoGroove.photoDecoder
                 |> Result.map .title
                 |> Expect.equal (Ok "(untitle)")
