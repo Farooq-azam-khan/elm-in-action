f62ca24b29e21b97198547a5f8414d2e85fcfec7
Farooq <farooq1.khan@ryerson.ca>
2022-06-22 09:57:14 -0400

    added lazy

--- src/Main.elm
+++ src/Main.elm
@@ -3,15 +3,53 @@ module Main exposing (main)
 import Browser exposing (Document)
 import Html exposing (Html, a, footer, h1, li, nav, text, ul)
 import Html.Attributes exposing (classList, href)
+import Html.Lazy exposing (lazy)
 
 
 type alias Model =
-    {}
+    { page : Page }
+
+
+type Page
+    = Gallery
+    | Folders
+    | NotFound
 
 
 view : Model -> Document Msg
 view model =
-    { title = "Photo Groove, SPA style", body = [ text "This is even my final form!" ] }
+    let
+        content =
+            text "This isn't even my fnal form!"
+    in
+    { title = "Photo Groove, SPA style"
+    , body = [ lazy viewHeader model.page, content, viewFooter ]
+    }
+
+
+viewHeader : Page -> Html Msg
+viewHeader page =
+    let
+        logo =
+            h1 [] [ text "Photo Groove" ]
+
+        links =
+            ul []
+                [ navLink Folders { url = "/", caption = "Folders" }
+                , navLink Gallery { url = "/gallery", caption = "Gallery" }
+                ]
+
+        navLink : Page -> { url : String, caption : String } -> Html msg
+        navLink targetPage { url, caption } =
+            li [ classList [ ( "active", page == targetPage ) ] ]
+                [ a [ href url ] [ text caption ] ]
+    in
+    nav [] [ logo, links ]
+
+
+viewFooter : Html msg
+viewFooter =
+    footer [] [ text "One is never alone with a rubber duck. -Douglas Adams" ]
 
 
 type Msg
@@ -31,7 +69,7 @@ subscriptoins model =
 main : Program () Model Msg
 main =
     Browser.document
-        { init = \_ -> ( {}, Cmd.none )
+        { init = \_ -> ( { page = Folders }, Cmd.none )
         , subscriptions = subscriptoins
         , update = update
         , view = view
